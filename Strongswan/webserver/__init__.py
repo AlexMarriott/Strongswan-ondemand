843193a2faad16b20f96d6fa5b5919d3229333d3
amarriott <alex-marriott@live.co.uk>
2022-01-05 19:50:54 +0000

    Added a login feature for when the code is hosted.

---
+++ Strongswan/webserver/__init__.py
@@ -0,0 +1,27 @@
+import os
+import uuid
+
+from flask import Flask, flash, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = uuid.uuid4().hex
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    db.init_app(app)
+    login_manager = LoginManager()  # Create a Login Manager instance
+    login_manager.init_app(app)  # configure it for login
+    from models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):  # reload user object from the user ID stored in the session
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    return app
--- Strongswan/webserver/main.py
+++ Strongswan/webserver/main.py
@@ -1,31 +1,62 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import render_template, request, redirect, url_for, flash, send_from_directory
+from flask_login import login_user, logout_user, login_required, current_user
+from api.digital_ocean_api import DigitalOceanApi
+from datetime import datetime
+from __init__ import create_app, db
+from models import User
+
+import os
 import sys
 import time
 
-from flask import Flask, render_template, request, redirect, url_for, flash,send_from_directory
-from api.digital_ocean_api import DigitalOceanApi
-from datetime import datetime
-
-import uuid
-import os
-import ansible_runner
-
-#from database import DataBase
-
-
-app = Flask(__name__)
-app.secret_key = uuid.uuid4().hex
+# import ansible_runner
 
 do = DigitalOceanApi()
+app = create_app()
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':  # if the request is a GET we return the login page
+        return render_template('login.html')
+    else:  # if the request is POST the we check if the user exist and with te right password
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it to the hashed password in the database
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('login'))
+
+        login_user(user)
+        return redirect(url_for('index'))
+
+
+@login_required
+@app.route('/logout')  # define logout path
+def logout():  # define the logout function
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@login_required
 @app.route('/')
 def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('.login'))
     node_list = refresh_servers()
     filename = 'server-cert.pem'
     return render_template('index.html', droplets=node_list, filename=filename)
+
 
 @app.route('/static/')
 def send_css():
     return app.send_static_file('website.css')
 
+
+@login_required
 @app.route('/api/refresh_servers')
 def refresh_servers():
     # Return  the status of all running servers
@@ -38,6 +69,8 @@ def refresh_servers():
                           'status': droplet['status']})
     return node_list
 
+
+@login_required
 @app.route('/api/create_server', methods=['GET'])
 def create_server():
     # Run the DO create server api
@@ -68,31 +101,34 @@ def create_server():
         fout.close()
 
         os.chmod('/tmp/id_rsa', 0o700)
-        #Run the ansible scripts using a ip call back + ssh key from create DO server
+        # Run the ansible scripts using a ip call back + ssh key from create DO server
         time.sleep(20)
         out, err, rc = ansible_runner.run_command(
-                    executable_cmd='ansible-playbook',
-                    cmdline_args=['strongswan.yml', '-i', 'hosts','-v', '-u',  'root'],
-                    host_cwd='../ansible',
-                    input_fd=sys.stdin,
-                    output_fd=sys.stdout,
-                    error_fd=sys.stderr,
-                )
+            executable_cmd='ansible-playbook',
+            cmdline_args=['strongswan.yml', '-i', 'hosts', '-v', '-u', 'root'],
+            host_cwd='../ansible',
+            input_fd=sys.stdin,
+            output_fd=sys.stdout,
+            error_fd=sys.stderr,
+        )
         print("rc: {}".format(rc))
         print("out: {}".format(out))
         print("err: {}".format(err))
         # Return the CA certificate + username + password
         stored_file_name = 'server-cert.pem'
         # TODO make a vpn key for each running server
-        #node_details['droplet']['id']
+        # node_details['droplet']['id']
         return redirect(url_for('index', filename=stored_file_name))
 
+
+@login_required
 @app.route('/api/vpncertdownload/<filename>')
 def vpncert_download(filename):
     return send_from_directory('/tmp/', filename)
 
 
-@app.route('/api/delete_server', methods=['GET','POST'])
+@login_required
+@app.route('/api/delete_server', methods=['GET', 'POST'])
 def delete_server():
     ids = request.form.getlist('ids')
     for dropletid in ids:
@@ -101,7 +137,14 @@ def delete_server():
     return redirect(url_for('index'))
 
 
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="80") #ssl_context="adhoc")
+    db.create_all(app=create_app())
+    # create admin user
+    with app.app_context():
+        user = User.query.filter_by(username=os.environ['ADMIN_USERNAME']).first()
+        if not user:
+            admin_user = User(username=os.environ['ADMIN_USERNAME'],
+                              password=generate_password_hash(os.environ['ADMIN_PASSWORD'], method='sha256'))
+            db.session.add(admin_user)
+            db.session.commit()
+    app.run(host="0.0.0.0", port="80")  # ssl_context="adhoc")
