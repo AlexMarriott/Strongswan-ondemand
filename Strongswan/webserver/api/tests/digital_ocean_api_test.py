953a64524f6fea1c858661c98f3bc1206a5514ee
amarriott <alex-marriott@live.co.uk>
2022-01-10 20:39:49 +0000

    Refactoring

---
+++ Strongswan/webserver/api/tests/digital_ocean_api_test.py
@@ -0,0 +1,21 @@
+from Strongswan.webserver.api import digital_ocean_api
+
+import unittest
+import json
+
+
+class DigitalOceanApiTest(unittest.TestCase):
+    @staticmethod
+    def setup():
+        pass
+
+    @staticmethod
+    def clean_up(nodes):
+        pass
+
+    def test_create_node(self):
+        pass
+
+    def test_delete_node(self):
+        pass
+
